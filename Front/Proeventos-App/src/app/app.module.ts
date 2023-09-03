cefaf79f90adc1c1f68193e44e44af32e5691cd1
unknown <farad.douglas@gmail.com>
2023-09-02 23:47:41 -0300

    Seção 5: Angular, interpolação, diretivas e binding

--- Front/Proeventos-App/src/app/app.module.ts
+++ Front/Proeventos-App/src/app/app.module.ts
@@ -1,24 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventosComponent,
-    PalestrantesComponent
+    PalestrantesComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front/Proeventos-App/src/app/eventos/eventos.component.ts
+++ Front/Proeventos-App/src/app/eventos/eventos.component.ts
@@ -8,8 +8,38 @@ import { HttpClient } from '@angular/common/http'
 })
 export class EventosComponent {
   public eventos: any;
+  public eventosFiltrados: any;
+  larguraImagem: number = 70;
+  margemImagem: number = 2;
+  exibirImagem: boolean = true;
+  private _filtroLista: string = '';
 
-  constructor(private http: HttpClient){}
+  public get filtroLista(){
+    return this._filtroLista
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value
+  
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos
+  }
+
+  filtrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+
+    var eventos =  this.eventos.filter(
+      (evento: any)=> {
+       return evento.tema.toLocaleLowerCase().indexOf(filtrarPor) != - 1 ||
+              evento.local.toLocaleLowerCase().indexOf(filtrarPor) != -1
+      }
+    )
+    
+    return eventos
+  }
+
+  constructor(private http: HttpClient){
+
+  }
 
   ngOnInit(){
     this.getEventos()
@@ -19,9 +49,13 @@ export class EventosComponent {
     this.http.get('https://localhost:5001/api/Evento').subscribe({
       next: (res) => {
         this.eventos = res
+        this.eventosFiltrados = this.eventos
         console.log(res)
       },
       error: (err) =>  console.log(err)
-  })
+  })}
+
+  public updateShowImage(input: boolean){
+    this.exibirImagem = input
   }
 }
